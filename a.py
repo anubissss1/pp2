421c3b2dc5a096b89ce3898e799c7cb9d2930b31
_anubissss_ <dinmukhammed.abdykadyr@gmail.com>
2024-01-24 19:56:03 +0600

    homework

--- a.py
+++ a.py
@@ -1,3 +1,74 @@
-print("Hi")
+#print('Hello, World')
 
+#if 5 > 2:
+#    print("YES")
+
+#This is a comment 
+
+"""
+This is a comment 
+written in 
+more than just one
+"""
+
+carname = "Volvo"
+# x = 50 
+
+#x = 5
+#y = 10
+#print(x+y)
+#z = x + y
+#print(z)
+
+#x,y,z = "Orange","Banana","Cherry"
+
+
+#x=y=z="Orange"
+
+
+#def myfunc():
+#    global x 
+#    x = "fantastic"
+
+#x = 5 
+#print(type(x))
+#type gives you the type of data
+
+
+# ("smth") - tuple
+# any numbers - int
+# 5.1 - float 
+# ["Orange","Cherry"] - list
+# "words" - str
+# {"name" : "John", "age" : 36} - dict 
+# True,False - bool 
+
+
+#x = 5 
+#x = float(x) - changing the type from int to float
+
+# x = "A lot of words"
+# print(len(x)) - gives you a number of symbols used in str
+
+# txt = "Hi"
+# x = txt[0]
+# x = txt[0:2]
+
+#txt = " HELLO WORLD "
+#x = txt.strip()
+#print(x)
+
+
+#txt = "hello world"
+#txt = txt.upper()
+
+#txt = "HELLO WORLD"
+#txt = txt.lower()
+
+#txt = "Hello World"
+#txt = txt.replace("H","W")
+
+#age = 36
+#txt = "My name is John Elton and I am {}"
+#print(txt.format(age))
 
