a06b37cb97085e52a34e623d94f8378a27db46a0
_anubissss_ <dinmukhammed.abdykadyr@gmail.com>
2024-02-09 20:12:06 +0600

    lab2

---
+++ lab2.py
@@ -0,0 +1,82 @@
+#print(bool("Hi"))
+#print(bool(993))
+
+
+#print(bool(0))
+
+
+
+#a = 50
+#b = 2
+
+#rint(a*b)
+#print(a/b)
+#print(float(b/a))
+#print(a**2)
+
+#if a == b:
+#    print("They are equally")
+#else:
+#    print("Not equally")
+
+#list = ["pinapple", "mango","strawberry"]
+#print(list[0])
+#print(list[-1])
+#list.append("tomato")
+#print(list[0:4])
+#list.insert(1,"banana")
+#print(list[0:5])
+
+#print(len(list))
+
+
+
+#berry = ("cherry",'strawberry')
+#print(berry[1])
+#berry.add("blueberry")
+
+#fruits = {"pinapple","apple"}
+
+#if 'apple' in fruits:
+#    print("Yes,it's fruit")
+#else:
+#    print("No,it isn't")
+
+#fruits.add("banana")
+#vegetables = {"tomato","potato","onion"}
+#fruits.update(vegetables)
+#fruits.remove('tomato')
+#fruits.discard("banana")
+#print(fruits)
+
+
+#car = {
+#    "brand": "Ford",
+#    "model": "Mustang",
+#    "year": 1964
+#}
+#print(car.get("model"))
+
+#car["year"] = 2020
+#car["color"] = "red"
+
+#car.pop('model')  - remove from dictionary
+#car.clear() - clear all from car(dictionary)
+#a,b,c = 10,20,10
+#if (a=b or b=c):
+#    print("Say Cheese!")
+
+c = 1
+while c<6:
+    print("Easy")
+    c+=1
+
+
+fruits = ["apple","banana","pinapple"]
+
+for x in fruits:
+    print(x)
+
+
+for x in range(6):
+    print(x)
